d1a203349b04d0600848ac75045fb371aafa9e22
Martin Harris <github@nakomis.com>
2014-04-29 10:45:10 +0100

    Added clustered deployment (cassandra datacenter & controlled dynamic web app)

--- src/main/java/io/cloudsoft/usergrid/UsergridTomcatServer.java
+++ src/main/java/io/cloudsoft/usergrid/UsergridTomcatServer.java
@@ -2,6 +2,7 @@ package io.cloudsoft.usergrid;
 
 import brooklyn.config.ConfigKey;
 import brooklyn.entity.basic.ConfigKeys;
+import brooklyn.entity.nosql.cassandra.CassandraDatacenter;
 import brooklyn.entity.proxying.ImplementedBy;
 import brooklyn.entity.webapp.tomcat.TomcatServer;
 import brooklyn.util.flags.SetFromFlag;
@@ -11,7 +12,12 @@ public interface UsergridTomcatServer extends TomcatServer {
     
     @SetFromFlag("cassandraUrl")
     public static final ConfigKey<String> CASSANDRA_URL = ConfigKeys.newStringConfigKey("usergrid.cassandra.url",
-        "URL of the Cassandra entity");
+        "URL of the Cassandra entity. Used when using a single cassandra node");
+    
+    @SetFromFlag("cassandraDatacenter")
+    public static final ConfigKey<CassandraDatacenter> CASSANDRA_DATACENTER = ConfigKeys.newConfigKey(CassandraDatacenter.class,
+        "usergrid.cassandra.datacenter",
+        "Cassandra data center to be used as the data store");
     
     @SetFromFlag("usergridPropertiesTemplateUrl")
     ConfigKey<String> USERGRID_PROPERTIES_TEMPLATE_URL = ConfigKeys.newStringConfigKey(
--- src/main/java/io/cloudsoft/usergrid/UsergridTomcatServerImpl.java
+++ src/main/java/io/cloudsoft/usergrid/UsergridTomcatServerImpl.java
@@ -1,6 +1,12 @@
 package io.cloudsoft.usergrid;
 
+import java.util.List;
+
+import com.google.common.base.Joiner;
+
 import brooklyn.entity.basic.Attributes;
+import brooklyn.entity.basic.Entities;
+import brooklyn.entity.nosql.cassandra.CassandraDatacenter;
 import brooklyn.entity.webapp.tomcat.TomcatServerImpl;
 import brooklyn.util.text.Strings;
 
@@ -9,9 +15,17 @@ public class UsergridTomcatServerImpl extends TomcatServerImpl implements Usergr
     @Override
     public String getCassandraUrl() {
         String url = getConfig(CASSANDRA_URL);
-        if (url == null)
-            return null;
-        return Strings.removeFromStart(url, "cassandra://");
+        CassandraDatacenter dataCenter = getConfig(CASSANDRA_DATACENTER);
+        if (!(url == null ^ dataCenter == null)) {
+            throw new IllegalStateException("Either cassandra url or datacenter must be set, but not both");
+        }
+        if (url == null) {
+            Entities.waitForServiceUp(dataCenter);
+            List<String> nodes = dataCenter.getAttribute(CassandraDatacenter.CASSANDRA_CLUSTER_NODES);
+            return Joiner.on(",").join(nodes);
+        } else {
+            return Strings.removeFromStart(url, "cassandra://");
+        }
     }
     
     @Override
